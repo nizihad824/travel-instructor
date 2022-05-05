e550f384418f831bb2b5dc0acbd51bdf009b99df
nizihad824 <mdnurulislam97@gmail.com>
2022-05-05 15:51:26 +0200

    header done

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Hi!This is me</h1>
+            
+        </div>
+    );
+};
+
+export default About;
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import logo from '../../../Images/logo.png'
+
+// import './Header.css'
+
+
+import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
 
 const Header = () => {
+   
     return (
         <div>
-            <h1>Welcome to Header</h1>
+            <Navbar sticky="top" bg="dark" variant="dark" collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Brand  to="/home/#top">
+                        <img
+                            src={logo}
+                            width="170"
+                            height="170"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" className='ms-5'>
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/appointment">Upcommig Trips</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About Me</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            {/* {user.email ?
+                                <Button onClick={handleSignOut} variant='light'>Sign-Out</Button> :
+                                <Nav.Link as={Link} to="/signin">Sign-In</Nav.Link>
+                            }
+                            {user.displayName &&
+                                <Navbar.Text className='ms-2'>
+                                    Signed in as: <a href="#">{user.displayName}</a>
+                                </Navbar.Text>
+                            } */}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
